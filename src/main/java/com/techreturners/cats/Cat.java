f6d0a4236220f9cd35e4fd85582b9b7eb546bb79
sairaawan <syraawan@live.com>
2022-01-22 17:58:24 +0000

    Reformatted code

--- src/main/java/com/techreturners/cats/Cat.java
+++ src/main/java/com/techreturners/cats/Cat.java
@@ -3,10 +3,15 @@ package com.techreturners.cats;
 interface Cat {
 
     public boolean isAsleep();
+
     public String getSetting();
+
     public String eat();
+
     public void wakeUp();
+
     public void goToSleep();
+
     public int getAverageHeight();
 
 
